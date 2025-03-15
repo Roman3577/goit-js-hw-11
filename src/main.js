2bd239c55819e5cdae11cac716a4da9ea27ee301
Roma3577 <mularr37@gmail.com>
2025-03-15 17:28:17 +0200

    task1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,45 @@
-import pixData from "./js/pixabay-api";
-import rendData from "./js/render-functions";
+import fetchImages from "./js/pixabay-api";
+import renderImages from "./js/render-functions";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast"; 
+import "izitoast/dist/css/iziToast.min.css"; 
+
+const form = document.querySelector(".form");
+const gallery = document.querySelector(".gallery");
+const loader = document.getElementById("loader");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250
+});
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    gallery.innerHTML = "";
+  const query = event.currentTarget.elements["search-text"].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: "Warning",
+      message: "Please enter a search term.",
+      position: "topRight"
+    });
+    return;
+  }
+  fetchImages(query).then(images => {
+    if (images.length > 0) {
+      renderImages(images, gallery);
+      lightbox.refresh();
+    } else {
+      iziToast.info({
+        title: "No Images",
+        message: "No images found for your query.",
+        position: "topRight"
+      });
+    }
+  }).catch(error => {
+
+    console.error("Error fetching images:", error);
+  });
+});
